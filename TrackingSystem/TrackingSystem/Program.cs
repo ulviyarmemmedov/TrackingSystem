e30729c464ec26183420d62e179b1c1adf4350e2
Your Name <girehip876@jobsfeel.com>
2024-02-21 13:42:03 +0400

    check

--- TrackingSystem/TrackingSystem/Program.cs
+++ TrackingSystem/TrackingSystem/Program.cs
@@ -10,6 +10,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
