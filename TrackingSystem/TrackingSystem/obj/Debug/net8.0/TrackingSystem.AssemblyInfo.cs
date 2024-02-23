35a1a50702dfa5fea2f33d6755ede39b06ad225b
insidecode-dev <trqtmhdv@gmail.com>
2024-02-23 11:13:10 +0400

    .gitignore file created

--- TrackingSystem/TrackingSystem/obj/Debug/net8.0/TrackingSystem.AssemblyInfo.cs
+++ TrackingSystem/TrackingSystem/obj/Debug/net8.0/TrackingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d6857f367fb7bbb4cb76be5f33fe4463c4306c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fbc2bcce710e2fcec276418f175c1d2595e8fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
